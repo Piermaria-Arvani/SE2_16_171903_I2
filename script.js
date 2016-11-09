c85889d2df1a4b854dbb7ecc4a11552090db601c
Arvani Piermaria (ACC0083386) <piermaria.arvani@sci-b106pc066x.unitn.it>
2016-11-09 14:12:13 +0100

    Prime funzioni

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-var list = ["apple", "pear", "peach", "banana", "cherry", "melon", "lemon", "strawberry", "mango", "papaya"];
 
-var tot = 0;
-var i = 0;
-var fruit = null;
-var insertFruit = [];
-
-while ( i < 5 ){
-   fruit = prompt('Try to find a fruit'); 
-   if(list.indexOf(fruit)>=0){
-       if(insertFruit.indexOf(fruit)<0 ){
-           tot+=10;
-           alert ("indovinato!");
-           insertFruit.push(fruit);
-        }
-        else
-            alert("hai già provato questo frutto");
+function inserisci(){
+    console.log("inserisci");
+    var table = document.getElementById("Oggetti");
+    var n = document.getElementById("Oggetti").rows[0].cells.length;
+    var items =  table.rows[0];
+    var valori = table.rows[1];
+    var item = document.getElementById("item").value;
+    var numero = parseInt(document.getElementById("numero_inserito").value);
+    var trovato = false;
+    var indice;
+    for ( i = 0; i < n; i++){
+        console.log(item);
+        if( items.cells[i].innerHTML == item){
+            trovato = true;
+            indice = i;
+        }   
     }
-    else
-        alert("Questo frutto non è nella lista");
-    i++;
- }
+    if (trovato){
+        var temp = parseInt(valori.cells[indice].innerHTML) + numero;
+        var x = document.getElementById("Oggetti").rows[1].cells;
+        x[indice].innerHTML = ""+temp;
+    }
+    else{
+        var new1 = items.insertCell(-1)
+        new1.innerHTML = item;
+        var new2 = valori.insertCell(-1)
+        new2.innerHTML = "" + numero;
+    }
+
+    nascondi();
+}
 
 
-alert("il tuo punteggio :" + tot);
+function nascondi() {
+     document.getElementById('aggiunta').style.display = "none";
+}
+
+
+
+function visualizza(){
+   document.getElementById('aggiunta').style.display = "block";
+}
+
