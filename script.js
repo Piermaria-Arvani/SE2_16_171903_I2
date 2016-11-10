e7eef4fea4fd7e3ed95c5eb950b9c535ac7f9a00
RefPier <pier.mn@hotmail.it>
2016-11-10 17:06:03 +0100

    nuove feature implementate

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 function inserisci(){
-    console.log("inserisci");
     var table = document.getElementById("Oggetti");
     var n = document.getElementById("Oggetti").rows[0].cells.length;
     var items =  table.rows[0];
@@ -8,14 +7,17 @@ function inserisci(){
     var item = document.getElementById("item").value;
     var numero = parseInt(document.getElementById("numero_inserito").value);
     var trovato = false;
+    var totale = 0;
     var indice;
+    
     for ( i = 0; i < n; i++){
-        console.log(item);
         if( items.cells[i].innerHTML == item){
             trovato = true;
             indice = i;
-        }   
+        } 
+        totale+= parseInt(valori.cells[i].innerHTML);
     }
+    
     if (trovato){
         var temp = parseInt(valori.cells[indice].innerHTML) + numero;
         var x = document.getElementById("Oggetti").rows[1].cells;
@@ -27,9 +29,17 @@ function inserisci(){
         var new2 = valori.insertCell(-1)
         new2.innerHTML = "" + numero;
     }
-
+    totale += parseInt(numero);
+    controllaCapacita(totale);
     nascondi();
 }
+
+
+function controllaCapacita(totale){
+    var max = parseInt(document.getElementById("capacity").value);
+    if (totale > max) alert("Il magazzino è pieno");
+}
+
 
 
 function nascondi() {
